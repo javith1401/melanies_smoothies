933407c4aabc8138c9c19ce00b1434df3e2a757e
javith1401 <javithunique@gmail.com>
2024-08-31 12:07:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,10 +34,10 @@ if ingredients_list:
         ingredients_string  += fruit_chosen + ' '
     #st.write(ingredients_string)
 
-    my_insert_stmt = """insert into smoothies.public.orders(ingredients)
+    #my_insert_stmt = """insert into smoothies.public.orders(ingredients)
              #values('""" + ingredients_string + """','"""+name_on_order+ """')"""
-    st.write(my_insert_stmt)
-    st.stop()
+    #st.write(my_insert_stmt)
+    #st.stop()
     time_to_insert = st.button('Submit order')
     
     if  time_to_insert:
